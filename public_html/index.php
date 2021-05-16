be062d484dc16435e013c0c43a59bae7c9f51e56
Phillip Navarrete <phillipvictor2000@gmail.com>
2021-05-16 16:37:51 +0100

    Created repo

---
+++ public_html/index.php
@@ -0,0 +1,52 @@
+<html>
+    <head>
+        <title>Phillip Navarrete | Home</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="Keywords" content="">
+        <meta name="Description" content="">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Exo+2&display=swap" rel="stylesheet">
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+        <link rel="manifest" href="favicon/site.webmanifest">
+        <link rel="stylesheet" href="styles/stylesheet.css" type="text/css">
+    </head>
+    <body>
+        <?php require 'header.php'; ?>
+        <!-- Splash header -->
+        <div class = "splash-container">
+            <img class = "splash-image-back" src="images/pexels-hilary-halliwell-38012.jpg">
+            <img class = "splash-image-front" src="images/image.jpg">
+            <h1 class = "splash-message">
+                Lol get gud
+            </h1>
+        </div>
+        <!-- About me section -->
+        <div class = "about-me-container">
+            <div class = "content-container">
+                <h3 class="subtitle">WHAT I DO</h3>
+                <p class = "about-me-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <img src = "images/parts/seperator_small_green.png">
+                <div class = "button-container">
+                    <button class = "about-button" onclick="window.print()">
+                        <img src ="images/icons/icon_print_grey.png"><p><b>RESUME</b></p>
+                    </button>
+                    <button class = "about-button" onclick="window.open('Phillip_Navarrete_Curriculum_Vitae.pdf')">
+                        <img src ="images/icons/icon_pdf_grey.png"><p><b>DOWNLOAD PDF</b></p>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <!-- Skills -->
+        <div class = "skills-container">
+
+        </div>
+
+    <br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br>
+    </body>
+</html>
---
+++ public_html/test.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" href="styles/stylesheet.css" type="text/css">
+<link href="https://fonts.googleapis.com/css2?family=Exo+2&display=swap" rel="stylesheet">
